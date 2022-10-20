8c4ea0db7b9aeb64071cedbb83148c8d84ddab50
James McNeil <mcneil_james@hotmail.com>
2022-10-20 14:02:00 +0100

    lesson 43: Controlled inputs

--- chapter_03/my-first-react-app/src/components/NewEventForm.jsx
+++ chapter_03/my-first-react-app/src/components/NewEventForm.jsx
@@ -6,20 +6,41 @@ const NewEventForm = () => {
   const [title, setTitle] = useState('')
   const [date, setDate] = useState('')
 
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(date)
+    resetForm()
+  }
+
   return (
-    <form className='new-event-form'>
+    <form className='new-event-form' onSubmit={onSubmit}>
       <label>
         <span>Event Title:</span>
-        <input type='text' onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type='text'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type='date' onChange={(e) => setDate(e.target.value)} />
+        <input
+          type='date'
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
       </label>
       <button type='submit'>Submit</button>
       <p>
         title - {title} | date - {date}
       </p>
+      <p onClick={() => resetForm()}>Reset Form</p>
     </form>
   )
 }
