16793f4febf29d9d2c453b4c693c11511d67b69f
James McNeil <mcneil_james@hotmail.com>
2022-10-20 11:09:50 +0100

    lesson 1: The onChange Event - complete

--- chapter_03/my-first-react-app/src/components/NewEventForm.jsx
+++ chapter_03/my-first-react-app/src/components/NewEventForm.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import './NewEventForm.css'
 
 const NewEventForm = () => {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+
   return (
     <form className='new-event-form'>
       <label>
         <span>Event Title:</span>
-        <input type='text' />
+        <input type='text' onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type='date' />
+        <input type='date' onChange={(e) => setDate(e.target.value)} />
       </label>
       <button type='submit'>Submit</button>
+      <p>
+        title - {title} | date - {date}
+      </p>
     </form>
   )
 }
