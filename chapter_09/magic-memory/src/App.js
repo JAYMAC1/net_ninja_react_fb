00cc919ad201de6f003118ac0d72d528e47bdf29
James McNeil <mcneil_james@hotmail.com>
2022-10-23 13:46:59 +0100

    lesson 60 creating card grid complete

--- chapter_09/magic-memory/src/App.js
+++ chapter_09/magic-memory/src/App.js
@@ -32,6 +32,17 @@ function App() {
     <div className='App'>
       <h1>Magic Match</h1>
       <button onClick={() => shuffleCards()}>New Game</button>
+      <div className='card-grid'>
+        {cards &&
+          cards.map((card) => (
+            <div key={card.id} className={card}>
+              <div>
+                <img className='front' src={card.src} alt='card front' />
+                <img className='back' src='/img/cover.png' alt='card back' />
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
