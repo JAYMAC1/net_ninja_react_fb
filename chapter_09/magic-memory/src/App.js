7abbfa00a807aeb12b8f276fd85227bc780c158c
James McNeil <mcneil_james@hotmail.com>
2022-10-23 14:44:00 +0100

    lesson 62 complete

--- chapter_09/magic-memory/src/App.js
+++ chapter_09/magic-memory/src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -28,14 +30,21 @@ function App() {
     shuffleCards()
   }, [])
 
-  console.log(cards, turns)
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   return (
     <div className='App'>
       <h1>Magic Match</h1>
       <button onClick={() => shuffleCards()}>New Game</button>
 
       <div className='card-grid'>
-        {cards && cards.map((card) => <SingleCard key={card.id} card={card} />)}
+        {cards &&
+          cards.map((card) => (
+            <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          ))}
       </div>
     </div>
   )
--- chapter_09/magic-memory/src/components/SingleCard.jsx
+++ chapter_09/magic-memory/src/components/SingleCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './SingleCard.css'
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (
     <>
       <div className='card'>
         <div>
           <img className='front' src={card.src} alt='card front' />
-          <img className='back' src='/img/cover.png' alt='card back' />
+          <img
+            className='back'
+            onClick={handleClick}
+            src='/img/cover.png'
+            alt='card back'
+          />
         </div>
       </div>
     </>
