72cd19d84bc472f248a1f4eb65089bec30542a18
James McNeil <mcneil_james@hotmail.com>
2022-10-24 20:47:47 +0100

    lesson 64 complete

--- chapter_09/magic-memory/src/App.js
+++ chapter_09/magic-memory/src/App.js
@@ -3,12 +3,12 @@ import SingleCard from './components/SingleCard'
 import './App.css'
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ]
 
 function App() {
@@ -40,7 +40,15 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('Match')
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
       } else {
         console.log('No Match')
       }
@@ -48,6 +56,7 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
+  console.log(cards)
   return (
     <div className='App'>
       <h1>Magic Match</h1>
