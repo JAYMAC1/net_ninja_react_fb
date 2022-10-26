abb3da4777df39f070ab18cd1d5bbf48fedaebd4
James McNeil <mcneil_james@hotmail.com>
2022-10-26 19:57:11 +0100

    lesson 70 React Router setup

---
+++ chapter_10/multipage-site/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore
+        voluptate quaerat eveniet accusamus, delectus ex hic minima consequuntur
+        facere voluptatem mollitia nobis molestias quis! Aliquam, qui! Illum,
+        recusandae molestiae.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ chapter_10/multipage-site/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore
+        voluptate quaerat eveniet accusamus, delectus ex hic minima consequuntur
+        facere voluptatem mollitia nobis molestias quis! Aliquam, qui! Illum,
+        recusandae molestiae.
+      </p>
+    </div>
+  )
+}
+
+export default Home
