acaa025e5952dfc41b7f00db3821c69e06b56bc4
James McNeil <mcneil_james@hotmail.com>
2022-10-27 23:57:46 +0100

    import useFetch

--- chapter_10/multipage-site/src/pages/Home.jsx
+++ chapter_10/multipage-site/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useFetch } from '../hooks/useFetch'
 
 const Home = () => {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch('http://localhost:3000/articles')
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore
-        voluptate quaerat eveniet accusamus, delectus ex hic minima consequuntur
-        facere voluptatem mollitia nobis molestias quis! Aliquam, qui! Illum,
-        recusandae molestiae.
-      </p>
+    <div className='home'>
+      <h2>Articles</h2>
+      {isPending && <div>Loading....</div>}
+      {error && <div> {error} </div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id}>
+            <p>{article.title}</p>
+          </div>
+        ))}
     </div>
   )
 }
