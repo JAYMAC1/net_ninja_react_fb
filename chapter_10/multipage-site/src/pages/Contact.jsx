5f5cf8ce1fe1aec27a3b4668265aec67df20ef76
James McNeil <mcneil_james@hotmail.com>
2022-10-28 13:28:13 +0100

    lesson 78 react router 5 complete

--- chapter_10/multipage-site/src/pages/Contact.jsx
+++ chapter_10/multipage-site/src/pages/Contact.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Contact = () => {
+  const queryString = useLocation().search
+  const urlParams = new URLSearchParams(queryString)
+  const name = urlParams.get('name')
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here....</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore
         voluptate quaerat eveniet accusamus, delectus ex hic minima consequuntur
