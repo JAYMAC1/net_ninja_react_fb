d96f4c591baebabe3b39251a9fded3957a244555
James McNeil <mcneil_james@hotmail.com>
2022-11-06 18:56:12 +0000

    lesson 82 fetching data complete

--- chapter_11/cooking-ninja/src/pages/home/Home.jsx
+++ chapter_11/cooking-ninja/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { useFetch } from '../../hooks/useFetch'
 // styles
 import './Home.css'
@@ -9,11 +10,11 @@ const Home = () => {
     error,
   } = useFetch(' http://localhost:3000/recipes')
   return (
-    <div>
-      {error && <div>{error}</div>}
-      {isPending && <div>Loading....</div>}
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading....</p>}
       {recipes &&
-        recipes.map((recipe) => <h1 key={recipe.id}>{recipe.title}</h1>)}
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
     </div>
   )
 }
