1b802173542232e21bffe1d704eaffd2c6530b96
James McNeil <mcneil_james@hotmail.com>
2022-11-07 20:22:47 +0000

    lesson 84 recipe challenge complete

--- chapter_11/cooking-ninja/src/pages/recipe/Recipe.jsx
+++ chapter_11/cooking-ninja/src/pages/recipe/Recipe.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 
 // styles
 import './Recipe.css'
 
 const Recipe = () => {
-  return <div>Recipe</div>
+  const { id } = useParams()
+  console.log(id)
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes/${id}`)
+  return (
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && <h2>{recipe.title}</h2>}
+    </div>
+  )
 }
 
 export default Recipe
