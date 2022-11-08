16bf083ea6e490e62a3662b8f198058493ddc9f6
James McNeil <mcneil_james@hotmail.com>
2022-11-08 20:07:17 +0000

    lesson 86 create recipe complete

--- chapter_11/cooking-ninja/src/pages/create/Create.jsx
+++ chapter_11/cooking-ninja/src/pages/create/Create.jsx
@@ -1,11 +1,57 @@
 // imported dependences
-import React from 'react'
+import React, { useState } from 'react'
 
 // styles
 import './Create.css'
 
 const Create = () => {
-  return <div>Create</div>
+  const [title, setTitle] = useState()
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        {/* ingredients goes here */}
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            type='text'
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking Time (minutes):</span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button className='btn'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Create
