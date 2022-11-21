8d35091d6d3858b801adf406cac191292c90ecc6
James McNeil <mcneil_james@hotmail.com>
2022-11-21 19:29:00 +0000

    lesson 98 Reducers & useReducer complete

--- chapter_11/cooking-ninja/src/context/ThemeContext.js
+++ chapter_11/cooking-ninja/src/context/ThemeContext.js
@@ -1,13 +1,27 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 // Context provider component
 export const ThemeContext = createContext()
 
 // Easy to incorporate custom logic
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, color: action.payload }
+    default:
+      return state
+  }
+}
 
 export function ThemeProvider({ children }) {
+  const [state, dispatch] = useReducer(themeReducer, { color: 'blue' })
+
+  const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color })
+  }
+
   return (
-    <ThemeContext.Provider value={{ color: 'blue' }}>
+    <ThemeContext.Provider value={{ ...state, changeColor }}>
       {children}
     </ThemeContext.Provider>
   )
