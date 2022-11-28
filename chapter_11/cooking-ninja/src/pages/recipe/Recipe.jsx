ca4188e10c294ed5af011a00417237a5b2b111d3
James McNeil <mcneil_james@hotmail.com>
2022-11-28 20:13:01 +0000

    lesson 107 - fetch single doc from collection

--- chapter_11/cooking-ninja/src/pages/recipe/Recipe.jsx
+++ chapter_11/cooking-ninja/src/pages/recipe/Recipe.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 import { useTheme } from '../../hooks/useTheme'
 
 // styles
@@ -9,11 +9,31 @@ import './Recipe.css'
 const Recipe = () => {
   const { id } = useParams()
   const { mode } = useTheme()
-  const {
-    data: recipe,
-    isPending,
-    error,
-  } = useFetch(`http://localhost:3000/recipes/${id}`)
+
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false)
+          setRecipe({ ...doc.data() })
+        } else {
+          setIsPending(false)
+          setError('Could not find this recipe data')
+        }
+      })
+      .catch((error) => {
+        setError(error.message)
+        setIsPending(false)
+      })
+  }, [id])
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className='error'>{error}</p>}
