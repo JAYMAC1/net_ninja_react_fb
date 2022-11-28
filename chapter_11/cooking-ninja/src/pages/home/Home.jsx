1e04baf71d18b392219a8bd4028431207eeca344
James McNeil <mcneil_james@hotmail.com>
2022-11-28 18:53:44 +0000

    lesson 105 fetching a firestorec ollection

--- chapter_11/cooking-ninja/src/pages/home/Home.jsx
+++ chapter_11/cooking-ninja/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-// custom hooks
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 
 // components
 import RecipeList from '../../components/RecipeList'
@@ -9,16 +8,42 @@ import RecipeList from '../../components/RecipeList'
 // styles
 import './Home.css'
 const Home = () => {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch(' http://localhost:3000/recipes')
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true)
+    projectFirestore
+      .collection('recipes')
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError('No recipes to load')
+          setIsPending(false)
+        } else {
+          let results = []
+          snapshot.docs.forEach((doc) => {
+            results.push({
+              id: doc.id,
+              ...doc.data(),
+            })
+            setData(results)
+            setIsPending(false)
+          })
+        }
+      })
+      .catch((error) => {
+        setError(error.message)
+        setIsPending(false)
+      })
+  }, [])
+
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading....</p>}
-      {recipes && <RecipeList recipes={recipes} />}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
