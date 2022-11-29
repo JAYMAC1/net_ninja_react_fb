4fb04159f48be920c8c110ed4a156b8f13124147
James McNeil <mcneil_james@hotmail.com>
2022-11-29 20:37:16 +0000

    lesson 108

--- chapter_11/cooking-ninja/src/components/RecipeList.jsx
+++ chapter_11/cooking-ninja/src/components/RecipeList.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Trashcan from '../assets/trashcan.svg'
+import { useHistory } from 'react-router-dom'
+import { projectFirestore } from '../firebase/config'
 
 // styles
 import './RecipeList.css'
@@ -9,11 +12,28 @@ import { useTheme } from '../hooks/useTheme'
 
 const RecipeList = ({ recipes }) => {
   const { mode } = useTheme()
+  const history = useHistory()
   console.log(recipes.length)
   if (recipes.length === 0) {
     return <div className='error'>No recipes to load....</div>
   }
 
+  const handleDelete = (id) => {
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log(id, ' has been deleted')
+      })
+      .catch((error) => {
+        console.log('Error removing document: ', error)
+      })
+    setTimeout(() => {
+      history.push('/')
+    }, 1000)
+    console.log(id)
+  }
   return (
     <div className='recipe-list'>
       {recipes.map((recipe) => (
@@ -22,6 +42,11 @@ const RecipeList = ({ recipes }) => {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className='delete'
+            src={Trashcan}
+            onClick={() => handleDelete(recipe.id)}
+          />
         </div>
       ))}
     </div>
